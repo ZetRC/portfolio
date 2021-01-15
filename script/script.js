aa4b2173ab7b9121d81973eec9a2cbc898499538
zetRC <zetrosas555@gmail.com>
2021-01-15 14:57:43 -0600

    beta-ver

---
+++ script/script.js
@@ -0,0 +1,50 @@
+
+$(document).ready(function(){
+  //initCarousel
+    $('.portfolioContent').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1
+    });
+
+    //initAutoscroll
+
+    const scrollTime=800
+
+    $("#homeButton").click(function() {
+      $([document.documentElement, document.body]).animate({
+          scrollTop: $("html").offset().top
+      }, scrollTime);
+    });
+
+    $("#aboutButton").click(function() {
+      $([document.documentElement, document.body]).animate({
+          scrollTop: $("#aboutSection").offset().top
+      }, scrollTime);
+    });
+
+    $("#portfolioButton").click(function() {
+      $([document.documentElement, document.body]).animate({
+          scrollTop: $("#portfolioSection").offset().top
+      }, scrollTime);
+    });
+
+    $("#contactButton").click(function() {
+      $([document.documentElement, document.body]).animate({
+          scrollTop: $("#contactSection").offset().top
+      }, scrollTime);
+    });
+    
+    console.log("yo!")
+
+    //init slidebar
+
+
+    $("#slidebar-btn").on("click",function(){
+      $("#navbar-cont").slideToggle()
+    })
+
+    $("#homeButton , #aboutButton , #portfolioButton , #contactButton").click(function(){
+      $("#navbar-cont").toggle()
+    })
+  });
