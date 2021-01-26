41d6c8a5ad8f217348c68662e71180405ed551b4
zetRC <zetrosas555@gmail.com>
2021-01-26 15:01:23 -0600

    fixed responsive issues

--- script/script.js
+++ script/script.js
@@ -1,49 +1,58 @@
 
 $(document).ready(function(){
-  //initCarousel
-    $('.portfolioContent').slick({
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 1
-    });
+  
+    var initCarousel=(()=>{
+        $('.portfolioContent').slick({
+          infinite: true,
+          slidesToShow: 3,
+          slidesToScroll: 1
+        });
+    
+    })
 
-    //initAutoscroll
 
-    const scrollTime=800
-
-    $("#homeButton").click(function() {
-      $([document.documentElement, document.body]).animate({
-          scrollTop: $("html").offset().top
-      }, scrollTime);
-    });
-
-    $("#aboutButton").click(function() {
-      $([document.documentElement, document.body]).animate({
-          scrollTop: $("#aboutSection").offset().top
-      }, scrollTime);
-    });
-
-    $("#portfolioButton").click(function() {
-      $([document.documentElement, document.body]).animate({
-          scrollTop: $("#portfolioSection").offset().top
-      }, scrollTime);
-    });
-
-    $("#contactButton").click(function() {
-      $([document.documentElement, document.body]).animate({
-          scrollTop: $("#contactSection").offset().top
-      }, scrollTime);
-    });
+    var initAutoscroll=(()=>{
+      
+      const scrollTime=800
+  
+      $("#homeButton").click(function() {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("html").offset().top
+        }, scrollTime);
+      });
+  
+      $("#aboutButton").click(function() {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#aboutSection").offset().top
+        }, scrollTime);
+      });
+  
+      $("#portfolioButton").click(function() {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#portfolioSection").offset().top
+        }, scrollTime);
+      });
+  
+      $("#contactButton").click(function() {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#contactSection").offset().top
+        }, scrollTime);
+      });
+    })
     
 
-    //init slidebar
-
-
-    $("#slidebar-btn").on("click",function(){
-      $("#navbar-cont").slideToggle()
+    var slideBarToggler=(()=>{
+      
+      $("#slidebar-btn").on("click",function(){
+        $("#navbar-cont").slideToggle()
+      })
+  
+      $("#homeButton , #aboutButton , #portfolioButton , #contactButton").click(function(){
+        $("#navbar-cont").toggle()
+      })
     })
 
-    $("#homeButton , #aboutButton , #portfolioButton , #contactButton").click(function(){
-      $("#navbar-cont").toggle()
-    })
+    initCarousel()
+    slideBarToggler()
+    initAutoscroll()
   });
