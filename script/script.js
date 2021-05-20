0d60a45ed1f0c2fe7e8abaeb5e9c62ea27904063
zetRC <zetrosas555@gmail.com>
2021-05-20 14:27:43 -0500

    -added new animations -changes in content -layout changes

--- script/script.js
+++ script/script.js
@@ -56,26 +56,33 @@ $(document).ready(function(){
       let projects=[{
         name:'<a href="https://zetrc.github.io/ZYA/" target="_blank" rel="noopener noreferrer">ZYA</a>',
         image:'<img src="./images/zyaCover.webp" alt="">',
-        description:"Check out this online store where you cand find all sort of stuff",
+        description:"Check out this online store where you cand find all kind of products",
         categories:'<div><i class="fas fa-cog"></i>dinamic content</div><div><i class="fas fa-palette"></i>Web design</div><div><i class="fas fa-plug"></i>API integration</div>'
       },
       {
         name:'<a href="https://zetrc.github.io/online_galery/" target="_blank" rel="noopener noreferrer">FWCOM</a>',
         image:'<img src="./images/fwcom-preview.webp" alt="">',
-        description:"This online galery will allow you to watch really cool and endless images.",
+        description:"This online galery will allow you to watch beautiful and endless images.",
         categories:'<div><i class="fas fa-cog"></i>dinamic content</div><div><i class="fas fa-palette"></i>Web design</div><div><i class="fas fa-plug"></i>API integration</div>'
+      },
+      {
+        name:'<a href="https://zetrc.github.io/re-test/" target="_blank" rel="noopener noreferrer">Rs page </a>',
+        image:'<img src="./images/rstest.webp" alt="">',
+        description:"Just a nice and simple template for real state purposes",
+        categories:'<div><i class="fas fa-cog"></i>dinamic content</div><div><i class="fas fa-palette"></i>Web design</div><div><i class="fas fa-border-all"></i>template material</div>'
       },
       {
         name:'<a href="https://zetrc.github.io/weather_app/" target="_blank" rel="noopener noreferrer">WEAP</a>',
         image:'<img src="./images/weapCover.png" alt="">',
-        description:"With this app you will be able to find out the weather in any location you want...and some other data as well",
+        description:"With this app you will be able to find out the weather in any location you want...and some other related data",
         categories:'<div><i class="fas fa-cog"></i>dinamic content</div><div><i class="fas fa-palette"></i>Web design</div><div><i class="fas fa-plug"></i>API integration</div>'
       },{
         name:'<a href="https://zetrc.github.io/stopwatch/" target="_blank" rel="noopener noreferrer">Timeria</a>',
         image:'<img src="./images/timeriaCover.png" alt="">',
-        description:"You will be able to use a really usefull stopwatch here",
-        categories:'<div><i class="fas fa-cog"></i>dinamic content</div><div><i class="fas fa-palette"></i>Web design</div><div><i class="fas fa-plug"></i>javascript app</div>'
-      }]
+        description:"In this site you will be able to use a really usefull stopwatch",
+        categories:'<div><i class="fas fa-cog"></i>dinamic content</div><div><i class="fas fa-palette"></i>Web design</div><div><i class="fab fa-js"></i>javascript app</div>'
+      }
+    ]
       let i=0
       const projectsNumber=projects.length-1
       $("#nextProject-btn").click(function(){
@@ -106,9 +113,53 @@ $(document).ready(function(){
       })
     })
 
+    const hoverSection = (()=>{
+      $.fn.isInViewport = function() {
+        var elementTop = $(this).offset().top;
+        var elementBottom = elementTop + $(this).outerHeight();
+    
+        var viewportTop = $(window).scrollTop();
+        var viewportBottom = viewportTop + $(window).height();
+    
+        return elementBottom > viewportTop && elementTop < viewportBottom;
+      };
+
+      $(window).on('resize scroll load', function() {
+        if ($('#homeSection').isInViewport()) {
+          $("#navbar").find(".active-selector").removeClass("active-selector");
+          $("#homeButton").addClass("active-selector")
+        }
+        if ($('#aboutContent').isInViewport()) {
+          $("#navbar").find(".active-selector").removeClass("active-selector");
+          $("#aboutButton").addClass("active-selector")
+        }
+        if ($('#portfolioContent').isInViewport()) {
+          $("#navbar").find(".active-selector").removeClass("active-selector");
+          $("#portfolioButton").addClass("active-selector")
+        }
+        if ($('#contactContent').isInViewport()) {
+          $("#navbar").find(".active-selector").removeClass("active-selector");
+          $("#contactButton").addClass("active-selector")
+        }
+        //runs animation on contact icon when it is visible in viewport
+        if ($('#msgIcon').isInViewport()) {
+          const rotateValue = "360deg"
+          $('#msgIcon').css({
+            '-webkit-transform' : 'rotateY(' + rotateValue + ')',
+            '-moz-transform'    : 'rotateY(' + rotateValue + ')',
+            '-ms-transform'     : 'rotateY(' + rotateValue + ')',
+            '-o-transform'      : 'rotateY(' + rotateValue + ')',
+            'transform'         : 'rotateY(' + rotateValue + ')'
+          });
+        }
+        
+      });
+  
+    })
 
     initProjectSlider()
     initCarousel()
     slideBarToggler()
     initAutoscroll()
+    hoverSection()
   });
